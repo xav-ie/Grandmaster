96ca40518ca4193477c361d3681e1930f75fad32
jamesheilberg <jaheilberg@gmail.com>
2021-12-02 18:46:12 -0500

    WIP - will need modifications

---
+++ src/main/java/grandmaster/Block.java
@@ -0,0 +1,28 @@
+package grandmaster;
+
+public class Block{
+//	this class will represent a piece on the chess board
+//	it needs to keep track of its position and its occupation status
+//	
+	
+	public int x;
+	public int y;
+	public Piece piece;
+	
+	public Block(int x, int y, Piece piece) {
+		this.x = x;
+		this.y = y;
+		this.piece = piece;
+	}
+	
+	public void setPiece(Piece p) {
+		// return true if piece was set
+		// return false if piece cannot be set
+		// check that 0 <= x < 8 and 0 <= y < 8
+		this.piece = p;
+	}
+	
+	public void clearPiece() {
+		this.piece = null;
+	}
+}
