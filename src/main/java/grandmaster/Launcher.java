39ad8ece34071178117d0137a4bf60ad74796f41
Xavier Ruiz <xruiz@bu.edu>
2021-12-01 23:28:18 -0500

    Setting up Maven and JavaFX

---
+++ src/main/java/grandmaster/Launcher.java
@@ -0,0 +1,9 @@
+package grandmaster;
+
+public class Launcher {
+    
+    public static void main(String[] args) {
+				Prompt.main(args);
+        HelloFX.main(args);
+    }
+}
---
+++ src/main/java/grandmaster/Prompt.java
@@ -0,0 +1,11 @@
+package grandmaster;
+
+
+class Prompt 
+{
+		public static void main(String[] args)
+		{
+				System.out.println("wooooooo");
+		}
+	
+}
