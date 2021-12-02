d8947af0b053ab334c6daa96962d28203df91ecc
jamesheilberg <jaheilberg@gmail.com>
2021-12-02 18:44:38 -0500

    partially completed: see TODO

---
+++ src/main/java/grandmaster/Rook.java
@@ -0,0 +1,40 @@
+package grandmaster;
+
+import java.lang.Math;
+
+public class Rook extends Piece {
+	
+	public Rook(String color)
+    {
+        super(color);
+    }
+	
+	@Override
+	public boolean movePossible(Block from, Block to, Board b) {
+		//bishop can move diagonally only, no jumping!
+		
+		//check if same block
+		if (from.equals(to)) {
+			return false;
+		}
+		
+		// check if landing block is same color
+		if (to.piece != null) {
+			if (to.piece.color.equals(from.piece.color)) {
+				return false;
+			}
+		}
+		
+		if (to.x == from.x) {
+			return true;
+		}
+		
+		if (to.y == from.y) {
+			return true;
+		}
+		
+		// TODO:
+		// check if a piece was jumped
+		
+		return false;
+	}
