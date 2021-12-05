e50945835719647b696d086927068c609eae39d6
James Heilberg <63997723+jamesheilberg@users.noreply.github.com>
2021-12-05 18:21:26 -0500

    tests

--- src/main/java/grandmaster/Launcher.java
+++ src/main/java/grandmaster/Launcher.java
@@ -5,5 +5,7 @@ public class Launcher {
         Game ourGame = new Game();
         ourGame.init_Game();
         System.out.println(ourGame.vis());
+        ourGame.playerMove(ourGame.in_turn_player, 1, 0, 2, 0);
+        System.out.println(ourGame.vis());
     }
 }
